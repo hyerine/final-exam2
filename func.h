e85935a12da20c8022f77feff455bc3c2b61458b
hyerine <janghr1225@naver.com>
2017-12-19 11:32:38 +0900

    func.h

---
+++ func.h
@@ -0,0 +1,15 @@
+#ifndef FUNC_H
+#define FINC_H
+#define SIZE 500
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int teach;
+char *input;
+char *output;
+char *len;
+FILE *fp;
+
+#endif
