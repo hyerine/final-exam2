c295459363b8323fa5a0583cb449447d31bae3e1
hyerine <janghr1225@naver.com>
2017-12-19 11:34:48 +0900

    main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "func.h"
+
+int main()
+{
+    printf("what is your name? \n");
+    scanf("%s",name);
+    getchar();
+
+    while(1)
+    {
+        if(!strcmp("bye",in))
+        {
+            printf("bye\n\n");
+            break;
+        }
+        if(!strcmp(buffer,""))
+        {
+            return_answer(output);
+        }
+        if(buffer = NULL)
+        {
+            printf("i don't know. please teach me.\n\n");
+            TEACH();
+        }
+    }
+}
